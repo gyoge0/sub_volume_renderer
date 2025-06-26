a21730e6a3dc0392d5c58a95011b8ce3249123b9
Yogesh Thambidurai <thambiduraiy@hhmi.org>
2025-06-26 10:43:09 -0400

    remove unused type alias for Float32Array

--- src/rbvr/__init__.py
+++ src/rbvr/__init__.py
@@ -1,6 +1,5 @@
 from ._geometry import Coordinate, Roi
 from ._manager import RingBufferManager, WorldCoordinateRingBufferManager
-from ._types import Float32Array
 from ._wobject import GlobalSparseVolume
 
 # we need to import the shader module so that the shader is registered
@@ -9,7 +8,6 @@ from ._shader import GlobalSparseVolumeShader  # noqa: F401 # isort: skip
 
 __all__ = [
     "GlobalSparseVolume",
-    "Float32Array",
     "Roi",
     "Coordinate",
     "RingBufferManager",
--- src/rbvr/_types.py
+++
@@ -1,4 +0,0 @@
-import numpy as np
-import numpy.typing as npt
-
-Float32Array = npt.NDArray[np.float32]
